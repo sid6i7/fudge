96154b40813f3d5e5ac85decfa13cc28aa583a67
Siddhant Jain <siddhantjain209@gmail.com>
2023-11-02 13:03:58 +0530

    add api call to fetch products

--- client/src/pages/ShopPage.js
+++ client/src/pages/ShopPage.js
@@ -1,24 +1,37 @@
 import React, { useState, useEffect, useContext } from "react";
 import { ShopFilter } from "../components/ShopFilter";
-import { allProducts } from "../data/products";
+import { fetchProducts } from "../data/products";
 import { ShopProductCard } from "../components/ShopProductCard";
 import { Button, ThemeProvider } from "@mui/material";
 import { theme } from "../themes/Buttons";
 
 export const ShopPage = () => {
   const [filters, setFilters] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState(allProducts);
-  const [loading, setLoading] = useState(false);
-
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [allProducts, setAllProducts] = useState([]);
 
   useEffect(() => {
+    fetchProducts()
+    .then((data) => {
+      setAllProducts(data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.error("Error fetching products: ", error);
+      setLoading(false);
+    });
+  }, []);
+
+  useEffect(() => {
+
     const newFilteredProducts = allProducts.filter((product) => {
       if(filters.length === 0) return true;
       else if(filters.includes(product.type)) return true;
       else return false;
     })
     setFilteredProducts(newFilteredProducts);
-  }, [filters])
+  }, [filters, allProducts]);
 
   const handleFilterChange = (newFilters) => {
     setFilters(newFilters);
